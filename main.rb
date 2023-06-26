ea41293d708087170dd28c6ab8a43e6697d3877d
NemusD <22637049-NemusD@users.noreply.replit.com>
2023-06-26 23:12:03 +0000

    Adversario automático

--- main.rb
+++ main.rb
@@ -3,12 +3,13 @@ puts "Bienvenido al juego piedra, papel y tijeras"
 puts "Elige una opción jugador_uno: "
 
 jugador_uno = gets.chomp.downcase
-#puts jugador_uno
+puts "El jugador uno eligió #{jugador_uno}"
 
 #Selección de jugador dos
-puts "Bienvenido al juego piedra, papel y tijeras"
-puts "Elige una opción jugador_dos: "
-jugador_dos = gets.chomp.downcase
+puts 
+opciones = ["piedra", "papel", "tijeras"]
+jugador_dos = opciones.sample
+puts "El jugador dos eligió #{jugador_dos}"
 
 if jugador_uno == jugador_dos
   puts"Es un empate!"
